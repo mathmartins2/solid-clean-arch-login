8e7504e5b08624c4895979b269cf59c9338205a8
mathmartins2 <dev.sincroni@gmail.com>
2022-11-23 13:08:11 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,17 +4,11 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: httpRequest): httpResponse | undefined {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-    if (!httpRequest.body.password) {
-      return badRequest(new MissingParamError('password'))
-    }
-    if (!httpRequest.body.passwordConfirmation) {
-      return badRequest(new MissingParamError('passwordConfirmation'))
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]){
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
