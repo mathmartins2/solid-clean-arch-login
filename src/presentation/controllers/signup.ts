27ef6f7e6a1a6cc3f84936238e5b1f4d9a00ef83
mathmartins2 <dev.sincroni@gmail.com>
2022-11-23 12:58:59 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,29 +1,30 @@
 
 import { httpResponse, httpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 export class SignUpController {
   handle (httpRequest: httpRequest): httpResponse | undefined {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MissingParamError('name')
       }
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email')
+        body: new MissingParamError('email')
       }
     }
     if (!httpRequest.body.password) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: password')
+        body: new MissingParamError('password')
       }
     }
     if (!httpRequest.body.passwordConfirmation) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: passwordConfirmation')
+        body: new MissingParamError('passwordConfirmation')
       }
     }
   }
