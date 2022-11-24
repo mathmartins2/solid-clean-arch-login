7b7b062454be4d369c159edf4762dfd93b0c43f4
mathmartins2 <dev.sincroni@gmail.com>
2022-11-24 11:45:23 -0300

    feat: ensure BcryptAdapter calls bcryot with correct values

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
     '.+\\.ts$': 'ts-jest'
   },
   modulePathIgnorePatterns: [
-    '<rootDir>/src/presentation/protocols/index.ts', '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts', '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts'
+    '<rootDir>/src/presentation/protocols/index.ts',
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts'
   ]
 }
