1a643b43d1a8341b30245e38bfe04a046a229a50
mathmartins2 <dev.sincroni@gmail.com>
2022-11-23 23:10:09 -0300

    refactor: add modulePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,7 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  modulePathIgnorePatterns: ['<rootDir>/src/presentation/protocols/index.ts', '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts']
+  modulePathIgnorePatterns: [
+    '<rootDir>/src/presentation/protocols/index.ts', '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts', '<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts'
+  ]
 }
