fc9e96c851ffe8132cb9909080f3753e302fc522
mathmartins2 <matheusgleiciel@gmail.com>
2022-12-06 00:42:07 -0300

    refactor: revoke class

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -30,7 +30,7 @@ export class DbAuthentication implements Authentication {
     const isValid = await this.hashComparer.compare(authentication.password, account.password)
     if (!isValid) return null
     const token = await this.encrypter.encrypt(account.id)
-    await this.updateAccessTokenRepository.update(account.id, accessToken)
+    await this.updateAccessTokenRepository.update(account.id, token)
     return token
   }
 }
