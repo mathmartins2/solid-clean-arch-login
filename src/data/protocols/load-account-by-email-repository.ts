c1b2cefccd13f0973183898ef365a75961812c70
mathmartins2 <dev.sincroni@gmail.com>
2022-12-05 22:41:54 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct values

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../../domain/models/account'
+
+export interface LoadAccounByEmailRepository {
+  load (email: string): Promise<AccountModel>
+}
---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,14 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+import { LoadAccounByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccounByEmailRepository
+  constructor (loadAccountByEmailRepository: LoadAccounByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authentication: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authentication.email)
+    return ''
+  }
+}
